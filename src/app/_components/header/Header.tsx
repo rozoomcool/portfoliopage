a9e265c64155736aac250c902bc5c6cd814346be
rozoomcool <rozoom.um@gmail.com>
2024-11-29 01:24:20 +0300

    remove menu button

--- src/app/_components/header/Header.tsx
+++ src/app/_components/header/Header.tsx
@@ -29,11 +29,11 @@ export default function Header() {
                             </div>
                         </ul>
                     </div>
-                    <button className={styles.menuButton} onClick={toggleDrawer}>
+                    {/* <button className={styles.menuButton} onClick={toggleDrawer}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width={24} height={24}>
                             <path fillRule="evenodd" d="M3 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 5.25Zm0 4.5A.75.75 0 0 1 3.75 9h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 9.75Zm0 4.5a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Zm0 4.5a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
                         </svg>
-                    </button>
+                    </button> */}
                 </nav>
 
                 {/* <div className={isDrawerOpen ? styles.drawerOpened : styles.drawerClosed}>
